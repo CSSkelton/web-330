884928787674b6071f9b4640c6e001b292afb536
CSSkelton <codysskelton@gmail.com>
2023-11-06 15:41:54 -0500

    week-3 assignment

---
+++ week-3/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Cody Skelton
+ * Date: 11.05.2023
+ * Program aggregates export statements from other JS files to be used in main program
+ * JS code sourced from Bellevue University's WEB 330 Program Requirements
+ */
+
+
+export * from "./appetizer.js";
+export * from "./beverage.js";
+export * from "./dessert.js";
+export * from "./main-course.js";
+export * from "./bill.js";
